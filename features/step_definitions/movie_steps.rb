8bacf0c228e6276002c5fe024e3b028a5fd7ae28
saasbook <saasbook@saasbook.(none)>
2014-11-05 15:03:28 -0400

    Uncheck Implemented

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -21,10 +21,17 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  flunk "Unimplemented"
+# HINT: use String#split to split up the rating_list, then
+# iterate over the ratings and reuse the "When I check..." or
+# "When I uncheck..." steps in lines 89-95 of web_steps.rb
+rating_list.split(%r{\s*,\s*}).each_with_index {
+|name, index|
+if(!index) #first element
+When %Q{I #{uncheck}check "ratings[#{name}]"}
+else
+And %Q{I #{uncheck}check "ratings[#{name}]"}
+end
+}
 end
 
 Then /I should see all the movies/ do
